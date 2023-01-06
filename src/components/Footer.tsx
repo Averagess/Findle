6b660a49fd9434539096e95a321ad73f5a7fa82a
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-06 08:52:03 +0200

    style changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,22 @@ import Clock from "./Clock";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p style={{marginLeft: "10px", fontFamily: "sans-serif", color: "white"}}>
+      <p
+        style={{
+          marginLeft: "10px",
+          fontFamily: "sans-serif",
+          color: "white",
+          userSelect: "none",
+        }}
+      >
         made by{" "}
-        <b>
-          <a style={{color: "#0645AD"}} href="https://github.com/Averagess" target="_blank">
-            me
-          </a>
-        </b>
+        <a
+          style={{ color: "#0645AD", textDecoration: "none", fontWeight: "bold" }}
+          href="https://github.com/Averagess"
+          target="_blank"
+        >
+          me
+        </a>
       </p>
       <Clock />
     </div>
