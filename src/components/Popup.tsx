8217deb459ef9b52bdb9ad2d5f2bd66a77900780
average666 <kiiskinenhenrik@gmail.com>
2023-01-06 07:12:55 +0200

    added share result, shortcut close, style changes

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import CloseButton from "./CloseButton";
 
 interface Props {
@@ -8,6 +9,44 @@ interface Props {
 }
 
 const Popup = ({ correctWord, guesses, closeModal, resetGame }: Props) => {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        console.log("escape was pressed. closing modal");
+        closeModal();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
+  const boardToClipboard = () => {
+    const boardString = guesses.map((guess, index) => {
+      const roundString = guess.split("").map((char, index) => {
+        if (char === correctWord[index]) {
+          return "🟩";
+        } else if (correctWord.includes(char)) {
+          return "🟧";
+        } else {
+          return "⬛";
+        }
+      });
+      return roundString.join("") + "\n";
+    });
+    navigator.clipboard.writeText(
+        `Daily Findle\n${new Date(Date.now()).toLocaleDateString()}\n\n${boardString.join("")}`
+      )
+      .then(() => {
+        alert("Daily Findle copied to your clipboard!");
+        console.log("copied to clipboard");
+      })
+      .catch((err) => console.log("error copying to clipboard", err));
+  };
+
   return (
     <div
       style={{
@@ -20,7 +59,7 @@ const Popup = ({ correctWord, guesses, closeModal, resetGame }: Props) => {
         alignItems: "center",
         position: "absolute",
         top: 0,
-        zIndex: 1
+        zIndex: 1,
       }}
     >
       <div
@@ -31,25 +70,28 @@ const Popup = ({ correctWord, guesses, closeModal, resetGame }: Props) => {
           backgroundColor: "rgb(25,45,40)",
           alignItems: "center",
           width: "40vw",
+          padding: "2.5vh",
           borderRadius: 10,
-          position: "relative"
+          position: "relative",
         }}
       >
-          <h1 >Game over!</h1>
-          <CloseButton
-            style={{
-              position: "absolute",
-              top: 15,
-              right: 15,
-            }}
-            onClick={closeModal}
-          />
+        {guesses[guesses.length - 1] === correctWord ? <h1>Nice one!</h1>:<h1>Oh well!</h1>}
+        <CloseButton
+          style={{
+            position: "absolute",
+            top: 15,
+            right: 15,
+          }}
+          title="close"
+          onClick={closeModal}
+        />
         <p>the correct word was: {correctWord} !</p>
         {guesses[guesses.length - 1] === correctWord && (
           <p>u got it right after {guesses.length} guesses!</p>
         )}
         <div style={{ display: "flex", gap: 25 }}>
-          <button onClick={resetGame}>Play again</button>
+          <button className="general-button" onClick={resetGame}>Play again</button>
+          <button className="general-button" title="copy to clipboard" onClick={boardToClipboard}>or share!</button>
         </div>
       </div>
     </div>
