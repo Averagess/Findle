15b7ff0ad250228d2e3de15fd2031d0538ce8fa0
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-06 07:11:01 +0200

    removed whitespace, enhanced readability

--- src/components/EmptyGrid.tsx
+++ src/components/EmptyGrid.tsx
@@ -1,15 +1,11 @@
 import CharContainer from "./CharContainer";
 
-
 const EmptyGrid = () => {
+  const EmptyElements: JSX.Element[] = new Array(5)
+    .fill(null)
+    .map((_, index) => <CharContainer key={index} type="default" />);
 
-  const EmptyElements: JSX.Element[] = new Array(5).fill(null).map((_, index) => <CharContainer key={index} type="default"/>);
-
-  return(
-    <div className="guessContainer">
-      {EmptyElements}
-    </div>
-  )
-}
+  return <div className="guessContainer">{EmptyElements}</div>;
+};
 
 export default EmptyGrid;
