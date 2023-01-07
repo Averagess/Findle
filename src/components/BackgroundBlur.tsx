3d5fe8d282bad76696c27ff4050c500c63b56e11
average666 <kiiskinenhenrik@gmail.com>
2023-01-07 08:04:51 +0200

    Created BackgroundBlur.tsx

---
+++ src/components/BackgroundBlur.tsx
@@ -0,0 +1,17 @@
+
+
+interface Props {
+  children: React.ReactNode;
+}
+
+
+
+const BackgroundBlur = ({ children }: Props) => {
+  return (
+    <div className="bg-blur">
+      {children}
+    </div>
+  );
+};
+
+export default BackgroundBlur;
