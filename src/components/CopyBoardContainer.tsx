5750c65793e9a147bf843fe9c1bebd6cd16ac538
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-07 07:39:34 +0200

    created CopyBoardContainer

---
+++ src/components/CopyBoardContainer.tsx
@@ -0,0 +1,60 @@
+import CloseButton from "./CloseButton";
+
+interface Props {
+  board: string[];
+  closeModal: () => void;
+}
+
+const CopyBoardContainer = ({ board, closeModal }: Props) => {
+  return(
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          position: "fixed",
+          minWidth: "fit-content",
+          minHeight: "fit-content",
+          height: "25vh",
+          width: "40vw",
+          borderRadius: "25px",
+          zIndex: 2,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "gray",
+          padding: "4.5vh"
+        }}
+      >
+        <div>
+          <CloseButton
+            title="close result"
+            style={{position: "absolute", top: 15, right: 15}}
+
+            onClick={() =>
+              closeModal()
+            }
+          />
+        </div>
+        <div
+          style={{
+            backgroundColor: "black",
+            margin: "5px",
+            padding: "15px",
+            borderRadius: "5px",
+            userSelect: "all",
+          }}
+        >
+          {board.map((text, index) => {
+            if (text)
+              return (
+                <p style={{ margin: 0 }} key={index}>
+                  {text}
+                </p>
+              );
+            else return <br key={index} />;
+          })}
+        </div>
+      </div>
+  )
+}
+
+export default CopyBoardContainer;
