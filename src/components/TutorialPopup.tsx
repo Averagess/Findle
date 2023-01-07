4507a5c50496c5bd2e9a58b94abc0c88b66ea720
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-07 13:18:34 +0200

    Created TutorialPopup.tsx

---
+++ src/components/TutorialPopup.tsx
@@ -0,0 +1,37 @@
+import CloseButton from "./CloseButton";
+import GuessGrid from "./GuessGrid";
+
+interface Props {
+  closePopup: () => void;
+}
+
+const TutorialPopup = ({ closePopup }: Props) => {
+  return (
+    <div style={{ alignItems: "normal" }} className="popup-container rises-up">
+      <CloseButton
+        title="close"
+        style={{ position: "absolute", top: 15, right: 15 }}
+        onClick={closePopup}
+      />
+      <h1 style={{ margin: 0 }}>How to play</h1>
+      <ul>
+        <li>Each guess must be a valid 5-letter Finnish word</li>
+        <li>
+          After guessing, game will show the result of your guess and change
+          colors accordingly
+        </li>
+      </ul>
+      <h2>Examples</h2>
+      <GuessGrid guessString="TASSU" correctString="TIPOW" />
+      <p>
+        The first letter of the guessed word is correct and on the correct spot.
+      </p>
+      <GuessGrid guessString="AAMUT" correctString="MSXER" />
+      <p>The third letter is in the correct word, but in a different place.</p>
+      <GuessGrid guessString="KESÄT" correctString="-----" />
+      <p>None of the letters are in the correct word.</p>
+    </div>
+  );
+};
+
+export default TutorialPopup;
