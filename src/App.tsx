b0623818c1ff2ef438771b3e2af27dbdf6190a45
average666 <kiiskinenhenrik@gmail.com>
2023-01-07 08:04:09 +0200

    Enhanced readability, added BgBlur component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Popup from "./components/Popup";
 import NavBar from "./components/NavBar";
 import Keyboard from "./components/Keyboard";
 import Footer from "./components/Footer";
+import BackgroundBlur from "./components/BackgroundBlur";
 
 import words from "./assets/words";
 import allowedChars from "./assets/allowedChars";
@@ -18,7 +19,7 @@ function App() {
   const [guesses, setGuesses] = useState<string[]>([]);
   const [gameOver, setGameOver] = useState<boolean>(false);
   const [modalOpen, setModalOpen] = useState<boolean>(false);
-  const [shouldShake, setShouldShake] = useState<boolean>(false)
+  const [shouldShake, setShouldShake] = useState<boolean>(false);
 
   const setupGame = () => {
     const randomWord = words[Math.floor(Math.random() * words.length)];
@@ -35,25 +36,28 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if(shouldShake) setTimeout(() => setShouldShake(false), 500)
-  })
+    if (shouldShake) setTimeout(() => setShouldShake(false), 500);
+  });
 
   if (!currentWord) return <h1>Loading word.....</h1>;
-
 
   const handleKeyDown = (key: string) => {
     const upperCaseKey = key.toUpperCase();
 
-    if (key.length === 1 && allowedChars.includes(upperCaseKey) && input.length < 5 && !gameOver) {
-      setInput(oldInput => oldInput + upperCaseKey);
+    if (
+      key.length === 1 &&
+      allowedChars.includes(upperCaseKey) &&
+      input.length < 5 &&
+      !gameOver
+    ) {
+      setInput((oldInput) => oldInput + upperCaseKey);
     } else if (upperCaseKey === "BACKSPACE" || upperCaseKey === "BACK") {
-      setInput(oldInput => oldInput.slice(0, oldInput.length - 1));
+      setInput((oldInput) => oldInput.slice(0, oldInput.length - 1));
     } else if (upperCaseKey === "ENTER" && input.length === 5) {
-
-      if(!words.includes(input)) {
-        console.log(`input ${input} not in wordlist`)
-        setShouldShake(true)
-        return
+      if (!words.includes(input)) {
+        console.log(`input ${input} not in wordlist`);
+        setShouldShake(true);
+        return;
       }
 
       setGuesses((old) => [...old, input]);
@@ -61,14 +65,9 @@ function App() {
     }
   };
 
-
   const PastGuessElements = guesses.map((guess, index) => {
     return (
-      <GuessGrid
-        key={index}
-        guessString={guess}
-        correctString={currentWord}
-      />
+      <GuessGrid key={index} guessString={guess} correctString={currentWord} />
     );
   });
 
@@ -79,36 +78,56 @@ function App() {
           .map((_, index) => <EmptyGrid key={index} />)
       : [];
 
-  if (!gameOver && !modalOpen && (PastGuessElements.length === 5 || guesses[guesses.length - 1] === currentWord)) {
+  if (
+    !gameOver &&
+    !modalOpen &&
+    (PastGuessElements.length === 5 ||
+      guesses[guesses.length - 1] === currentWord)
+  ) {
     setTimeout(() => {
-      setModalOpen(true)
+      setModalOpen(true);
       setGameOver(true);
     }, 3500);
   }
 
-
   return (
-    <div className="App" onKeyDown={(event) => handleKeyDown(event.key)} tabIndex={0}>
+    <div
+      className="App"
+      onKeyDown={(event) => handleKeyDown(event.key)}
+      tabIndex={0}
+    >
       <NavBar />
       <div className="main-container">
         {/* <h1 style={{ color: "white", fontSize: "24px" }}>[ {currentWord} ]</h1> */}
         {PastGuessElements}
-        {PastGuessElements.length < 5 && <InputGrid value={input} shouldAnimateShake={shouldShake} />}
+        {PastGuessElements.length < 5 && (
+          <InputGrid value={input} shouldAnimateShake={shouldShake} />
+        )}
         {EmptyGuessElements}
         <Keyboard
           correctString={currentWord}
           guesses={guesses}
           changeInput={handleKeyDown}
-          />
-        {gameOver && <button style={{marginTop: "15px"}} className="general-button" onClick={() => setModalOpen(true)}>show results</button>}
+        />
+        {gameOver && (
+          <button
+            style={{ marginTop: "15px" }}
+            className="general-button"
+            onClick={() => setModalOpen(true)}
+          >
+            show results
+          </button>
+        )}
       </div>
       {modalOpen && (
-        <Popup
-          correctWord={currentWord}
-          guesses={guesses}
-          closeModal={() => setModalOpen(false)}
-          resetGame={setupGame}
-        />
+        <BackgroundBlur>
+          <Popup
+            correctWord={currentWord}
+            guesses={guesses}
+            closeModal={() => setModalOpen(false)}
+            resetGame={setupGame}
+          />
+        </BackgroundBlur>
       )}
       <Footer />
     </div>
